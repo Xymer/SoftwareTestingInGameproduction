7830f0e8d4e97cab6500fbe68c35f2988e1ecfc8
Xymer <sebastian.unland@futuregames.nu>
2020-08-21 11:50:40 +0200

    Init Push
    Adding 5 tests for pickups

---
+++ ShooterGame/Source/ShooterGame/Private/Pickups/AmmoPickupTest.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ShooterGame.h"
+#include "AmmoPickupTest.h"
+#include "Tests\AutomationCommon.h"
+#include "Tests\AutomationEditorCommon.h"
+
+AmmoPickupTest::AmmoPickupTest()
+{
+}
+
+AmmoPickupTest::~AmmoPickupTest()
+{
+}
